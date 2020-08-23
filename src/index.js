d1e0b57bcc92126eb477bc4e9092445ed42fe05c
jignesh88 <jignesh.282@gmail.com>
2020-08-24 00:44:36 +1000

    compose enhanceer compose enhanceer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import App from "./App";
 import { Provider } from "react-redux";
 import reducers from "./store/reducers";
@@ -15,8 +15,8 @@ const logger = (store) => {
     };
   };
 };
-
-const store = createStore(reducers, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(logger)));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
