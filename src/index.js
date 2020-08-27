8a6f6d84b4a3931d3917067b111804025c48c4ad
jignesh88 <jignesh.282@gmail.com>
2020-08-27 12:54:27 +1000

    fixes fixes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import App from "./App";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./store/reducers";
 
@@ -16,7 +17,10 @@ const logger = (store) => {
   };
 };
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
